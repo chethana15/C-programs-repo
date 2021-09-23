1619088b78cc016c582216fb9027e37caad1085d
chethana15 <90371115+chethana15@users.noreply.github.com>
2021-09-23 14:21:59 +0530

    Update missing_element_in_an_array.c

--- missing_element_in_an_array.c
+++ missing_element_in_an_array.c
@@ -3,11 +3,14 @@
 int main(void) 
 {
 	// your code goes here
-	int a[4]={17,24,38,45};
+	int a[]={10,12,14,18,20};
+	int y=sizeof(a);
+	int z=sizeof(a[0]);
+	int l=y/z;
 	int a1,d,k,i;
 	a1=a[0];
-	d=(a[3]-a1)/4;
-	for(i=1;i<5;i++)
+	d=(a[l-1]-a1)/4;
+	for(i=1;i<l;i++)
 	{
 	    k=a1+(i*d);
 	    if(k!=a[i])
