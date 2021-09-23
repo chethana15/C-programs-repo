fef683772daa15de5dee38fdd1107ca48795d753
chethana15 <90371115+chethana15@users.noreply.github.com>
2021-09-23 14:45:15 +0530

    Update second largest in array general.c

--- second largest in array general.c
+++ second largest in array general.c
@@ -1,36 +1,26 @@
-#include <stdio.h>
-
 int main(void) 
 {
 	// your code goes here
-	int A[]={10,20,30,40,50};
-	//int A[]={15,13,17,21,40,18};
-    int k=sizeof(A);
-    int m=sizeof(A[0]);
-    int l=0;
-    l=k/m;
-    printf("%d\n",max2(A,l));
+	int a[]={10,20,30,40,50};
+	int y=sizeof(a);
+	int z=sizeof(a[0]);
+	int l=y/z;
+	int max1,max2,i;
+	max1=max2=a[0];
+	
+	for(i=1;i<l;i++)
+	{
+	    if(max1<a[i])
+	    {
+	        max2=max1;
+	        max1=a[i];
+	    }
+	    else if(max2<a[i] && a[i]!=max1)
+	    {
+	        max2=a[i];
+	    }
+	}
+	printf("%d\n",max2);
 	return 0;
 }
-int max2(int a[],int size)
-{
-    int i,j,temp,count;
-    for(i=0;i<size;i++)
-    {
-        count=0;
-        for(j=i+1;j<size;j++)
-        {
-            if(a[i]<a[j])
-            {
-            temp=a[i];
-            a[i]=a[j];
-            a[j]=temp;
-            }
-           
-        }
-        count++;
-            if(count==2)
-            break;
-    }
-    return a[1];
-}
+
